6b5ec730f5966ae097f8413aedd64010d3bf5c6e
Muhammed Abu Shadi <muhammed.samir.abushadi@gmail.com>
2023-05-09 10:53:12 +0300

    Add new button with action

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,12 +12,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageViewOne: UIImageView!
     
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        diceImageViewOne.image = UIImage(imageLiteralResourceName: "")
+        diceImageViewOne.image = UIImage(named: "DiceSix" );
+        diceImageViewTwo.image = UIImage(named: "DiceTwo" );
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped.");
+        diceImageViewOne.image = UIImage(named: "DiceOne");
+        diceImageViewTwo.image = UIImage(named: "DiceThree");
+    }
+    
 }
 
